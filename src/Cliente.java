597e1d604eacc99e581d51daa029923462a9dd87
SantiagoUrrea2402 <santiurrea2006@gmail.com>
2026-04-10 17:42:52 -0500

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,53 +1,46 @@
 public class Cliente {
     protected String nombre;
-    protected int Identificacion;
+    protected int identificacion;
     protected double valorCompra;
-
 
     public Cliente() {
     }
 
     public Cliente(String nombre, int identificacion, double valorCompra) {
         this.nombre = nombre;
-        Identificacion = identificacion;
+        this.identificacion = identificacion;
         this.valorCompra = valorCompra;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public int getIdentificacion() {
-        return Identificacion;
-    }
-
-    public void setIdentificacion(int identificacion) {
-        Identificacion = identificacion;
+        return identificacion;
     }
 
     public double getValorCompra() {
         return valorCompra;
     }
 
-    public void setValorCompra(double valorCompra) {
-        this.valorCompra = valorCompra;
-    }
 
-    @Override
-    public String toString() {
-        return "Cliente{" +
-                "nombre='" + nombre + '\'' +
-                ", Identificacion=" + Identificacion +
-                ", valorCompra=" + valorCompra +
-                '}';
-    }
-
-    public double calcularValorCompra(double valorCompra){
+    public double calcularTotalPagar() {
         return valorCompra;
     }
-}
 
+    public double descontar(double porcentaje) {
+        return valorCompra - (valorCompra * porcentaje);
+    }
+
+    public double aumentar(double porcentaje) {
+        return valorCompra + (valorCompra * porcentaje);
+    }
+
+    public boolean compraAlta() {
+        return valorCompra > 300000;
+    }
+
+    public void imprimirResumen(double total) {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("ID: " + identificacion);
+        System.out.println("Valor original: " + valorCompra);
+        System.out.println("Total a pagar: " + total);
+    }
+}
